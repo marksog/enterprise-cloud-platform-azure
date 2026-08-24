2ad5437cc325601e33c9d7e5c7306e510d7f360b
Mark Meyof Kweyui Ngongang <meyofmarksog@gmail.com>
2026-08-24 16:26:38 -0400

    initial bootstrap comple

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -5,13 +5,13 @@ resource "azurerm_resource_group" "bootstrap" {
 }
 
 resource "azurerm_storage_account" "bootstrap" {
-  name                          = local.storage_account_name
-  resource_group_name           = azurerm_resource_group.bootstrap.name
-  location                      = azurerm_resource_group.bootstrap.location
-  account_tier                  = var.account_tier
-  account_replication_type      = var.account_replication_type
-  https_traffic_only_enabled    = var.https_traffic_only_enabled
-  public_network_access_enabled = true
+  name                            = local.storage_account_name
+  resource_group_name             = azurerm_resource_group.bootstrap.name
+  location                        = azurerm_resource_group.bootstrap.location
+  account_tier                    = var.account_tier
+  account_replication_type        = var.account_replication_type
+  https_traffic_only_enabled      = var.https_traffic_only_enabled
+  public_network_access_enabled   = true
   allow_nested_items_to_be_public = false
 
   network_rules {
