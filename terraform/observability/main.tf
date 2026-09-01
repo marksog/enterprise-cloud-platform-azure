e943abed74b2e52c676c0bb0d3ecf13644f5dfa3
Mark Meyof Kweyui Ngongang <meyofmarksog@gmail.com>
2026-09-01 14:30:19 -0400

    feat: centralize subscription activity logs

--- terraform/observability/main.tf
+++ terraform/observability/main.tf
@@ -23,3 +23,69 @@ resource "azurerm_log_analytics_workspace" "central" {
     CostCenter  = "platform"
   }
 }
+
+# Production subscription activity logs
+resource "azurerm_monitor_diagnostic_setting" "production_activity_logs" {
+  name                       = "prod-activity-logs-to-law"
+  target_resource_id         = "/subscriptions/e3bda1e9-e6e9-45a5-b2ee-d3d7a754b594"
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.central.id
+
+  provider = azurerm.prod
+
+  enabled_log {
+    category = "Administrative"
+  }
+
+  enabled_log {
+    category = "Security"
+  }
+
+  enabled_log {
+    category = "ServiceHealth"
+  }
+
+  enabled_log {
+    category = "Alert"
+  }
+
+  enabled_log {
+    category = "Recommendation"
+  }
+
+  enabled_log {
+    category = "Policy"
+  }
+}
+
+# Non-Production subscription activity logs
+resource "azurerm_monitor_diagnostic_setting" "nonproduction_activity_logs" {
+  name                       = "nonprod-activity-logs-to-law"
+  target_resource_id         = "/subscriptions/4eeb7aa0-684a-473d-9ae3-f238dfc3f944"
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.central.id
+
+  provider = azurerm.nonprod
+
+  enabled_log {
+    category = "Administrative"
+  }
+
+  enabled_log {
+    category = "Security"
+  }
+
+  enabled_log {
+    category = "ServiceHealth"
+  }
+
+  enabled_log {
+    category = "Alert"
+  }
+
+  enabled_log {
+    category = "Recommendation"
+  }
+
+  enabled_log {
+    category = "Policy"
+  }
+}
--- terraform/observability/providers.tf
+++ terraform/observability/providers.tf
@@ -3,3 +3,19 @@ provider "azurerm" {
 
   subscription_id = "1a8e995d-4fc1-485a-b192-562826ca1fc8"
 }
+
+provider "azurerm" {
+  alias = "prod"
+
+  features {}
+
+  subscription_id = "e3bda1e9-e6e9-45a5-b2ee-d3d7a754b594"
+}
+
+provider "azurerm" {
+  alias = "nonprod"
+
+  features {}
+
+  subscription_id = "4eeb7aa0-684a-473d-9ae3-f238dfc3f944"
+}
