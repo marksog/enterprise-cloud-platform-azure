10a80add121574415398187300c34ce44ccc9bea
Mark Meyof Kweyui Ngongang <meyofmarksog@gmail.com>
2026-09-11 00:38:45 -0400

    refactor: readdress paid connectivity hub

--- terraform/connectivity/main.tf
+++ terraform/connectivity/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "hub" {
   name                = "sog-hub-vnet"
   location            = azurerm_resource_group.hub.location
   resource_group_name = azurerm_resource_group.hub.name
-  address_space       = ["10.0.0.0/20"]
+  address_space       = ["10.30.0.0/20"]
 
   tags = {
     Environment = "platform"
