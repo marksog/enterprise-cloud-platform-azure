d20a66641d41960c38085cef41735b6a0b0b74b8
Mark Meyof Kweyui Ngongang <meyofmarksog@gmail.com>
2026-09-14 11:31:51 -0400

    feat: add shared services subscription to platform

--- terraform/subscriptions/output.tf
+++ terraform/subscriptions/output.tf
@@ -13,3 +13,15 @@ output "nonprod_network_resource_group_name" {
 output "nonprod_network_resource_group_location" {
   value = azurerm_resource_group.nonprod_network.location
 }
+
+output "platform_shared_services_subscription_id" {
+  value = "/subscriptions/9ee656be-a9d7-4006-b90f-7549517b1edc"
+}
+
+output "platform_connectivity_paid_subscription_id" {
+  value = "/subscriptions/2dcbd775-c87b-4099-bb4b-2740850fda0b"
+}
+
+output "platform_observability_subscription_id" {
+  value = "/subscriptions/1a8e995d-4fc1-485a-b192-562826ca1fc8"
+}
